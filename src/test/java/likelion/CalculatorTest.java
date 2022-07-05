17379e41875232048174a306b39a9a2d0db62d42
Jwhyee <kawnsdud@naver.com>
2022-07-05 23:59:09 +0900

    TDD 연습 추가

---
+++ src/test/java/likelion/CalculatorTest.java
@@ -0,0 +1,49 @@
+package likelion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    /*
+"1 + 1"
+"1 + 2"
+"1 - 2"
+"1 * 2"
+"4 / 2"
+"1 + 2 + 3"
+"1 + 2 * 3"
+"(1 + 2) * 3"
+*/
+    @Test
+    void add1() {
+        int rs = Calculator.calc("1 + 1");
+        assertEquals(2, rs);
+    }
+    @Test
+    void add2() {
+        int rs = Calculator.calc("1 + 2");
+        assertEquals(3, rs);
+    }
+    @Test
+    void add3() {
+        int rs = Calculator.calc("1 - 2");
+        assertEquals(-1, rs);
+    }
+    @Test
+    void add4() {
+        int rs = Calculator.calc("1 * 2");
+        assertEquals(2, rs);
+    }
+    @Test
+    void add5() {
+        int rs = Calculator.calc("4 / 2");
+        assertEquals(2, rs);
+    }
+    @Test
+    void add6() {
+        int rs = Calculator.calc("1 + 2 + 3");
+        assertEquals(6, rs);
+    }
+
+}
