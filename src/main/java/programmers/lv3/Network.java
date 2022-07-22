6ae3d94ae241aae66f5b6aacc72dee8dbbd04bbe
Jwhyee <kawnsdud@naver.com>
2022-07-22 14:22:21 +0900

    [PRG] 네트워크 / LV3 - 입출력 예 입력

--- src/main/java/programmers/lv3/Network.java
+++ src/main/java/programmers/lv3/Network.java
@@ -1,7 +1,16 @@
 package programmers.lv3;
 
 public class Network {
-    public static void main(String[] args) {
+    public static int solution(int n, int[][] computers) {
+        int answer = 0;
+        return answer;
+    }
 
+    public static void main(String[] args) {
+        int n = 3;
+//        int n = 3;
+        int[][] computer = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+//        int[][] computer = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+        System.out.println(solution(n, computer));
     }
 }
