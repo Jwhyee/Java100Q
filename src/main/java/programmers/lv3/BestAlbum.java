321d0a575709c3b21a0a342629300751e5d2b7cd
Jwhyee <kawnsdud@naver.com>
2022-09-05 18:26:41 +0900

    [PRG] 베스트앨범 - LV3

--- src/main/java/programmers/lv3/BestAlbum.java
+++ src/main/java/programmers/lv3/BestAlbum.java
@@ -10,6 +10,7 @@ import java.util.*;
  * 1. 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
  * 2. 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
  * 3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+ * 4. 장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
  */
 /* 풀이 순서
  * 1. 장르 별로 총 얼마나 재생되었는지 구하기
@@ -18,38 +19,52 @@ import java.util.*;
  * */
 public class BestAlbum {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> playCountMap = new HashMap<>();
+        System.out.println("genres = " + Arrays.toString(genres));
+        System.out.println("plays = " + Arrays.toString(plays));
+        Map<String, Integer> playCountMap = new HashMap<>();
         for (int i = 0; i < genres.length; i++) {
             // 장르별 총 재생 수
             playCountMap.put(genres[i], playCountMap.getOrDefault(genres[i], 0) + plays[i]);
         }
+
+        System.out.println("playCountMap = " + playCountMap);
+
+        // countMap의 keySet으로 된 List를 만들어 재생 횟수가 가장 높은 순서대로 정렬
         List<String> genresList = new ArrayList<>(playCountMap.keySet());
-        Collections.sort(genresList, (o1, o2) -> playCountMap.get(o2) - playCountMap.get(o1));
+        genresList.sort((o1, o2) -> playCountMap.get(o2) - playCountMap.get(o1));
+        System.out.println("genresList = " + genresList);
+
+        //
         List<Integer> countList = new ArrayList<>();
         for (int i = 0; i < genresList.size(); i++) {
-            String g = genresList.get(i);
+            String genre = genresList.get(i);
             int max = 0;
             int firstIdx = -1;
+
+            // 해당 장르의 음악 중에서 play 횟수가 첫번째로 큰 인덱스틀 찾기
             for(int j = 0; j < genres.length; j++) {
-                if(g.equals(genres[j]) && max < plays[j]) {
+                if(genre.equals(genres[j]) && max < plays[j]) {
                     max = plays[j];
                     firstIdx = j;
                 }
             }
 
-            //해당 장르의 음악 중에서 play횟수가 두번째로 큰 인덱스를 찾는다.
+            // 두번째로 큰 인덱스 찾기
             max = 0;
             int secondIdx = -1;
             for(int j = 0; j < genres.length; j++) {
-                if(g.equals(genres[j]) && max < plays[j] && j != firstIdx) {
+                if(genre.equals(genres[j]) && max < plays[j] && j != firstIdx) {
                     max = plays[j];
                     secondIdx = j;
                 }
             }
 
             countList.add(firstIdx);
-            if(secondIdx >= 0) countList.add(secondIdx); //secondIdx는 존재 할수도, 안할수도 있다.
+
+            // 두번째 인덱스가 존재하면 list에 추가
+            if(secondIdx >= 0) countList.add(secondIdx);
         }
+        System.out.println("countList = " + countList);
 
         int[] answer = new int[countList.size()];
         for(int i = 0; i < countList.size(); i++) {
