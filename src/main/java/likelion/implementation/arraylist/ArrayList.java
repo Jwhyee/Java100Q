6a6cb4b72dcab563d31f882c0e145b0f8a0cb666
Jwhyee <kawnsdud@naver.com>
2022-12-28 23:33:55 +0900

    add : List, Map 구현

---
+++ src/main/java/likelion/implementation/arraylist/ArrayList.java
@@ -0,0 +1,63 @@
+package likelion.implementation.arraylist;
+
+public class ArrayList<T> {
+    private Object[] datum;
+    private int size;
+
+    ArrayList() {
+        datum = new Object[2];
+        size = 0;
+    }
+
+    public void add(Object data) {
+        sizeUpIfFull();
+        datum[size] = data;
+        size++;
+    }
+
+    public void sizeUpIfFull() {
+        if (isFull()) {
+            sizeUp();
+        }
+    }
+
+    private void sizeUp() {
+        Object[] newDatum = new Object[datum.length * 2];
+
+        for ( int i = 0; i < datum.length; i++ ) {
+            newDatum[i] = datum[i];
+        }
+
+        datum = newDatum;
+    }
+
+    public boolean isFull() {
+        return size == datum.length;
+    }
+
+    public T get(int idx) {
+        return (T)datum[idx];
+    }
+
+    public void removeAt(int idx) {
+        for (int i = idx + 1; i < size; i++) {
+            datum[i - 1] = datum[i];
+        }
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int getArrayLength() {
+        return datum.length;
+    }
+
+    public void showAllValues() {
+        System.out.println("== 전체 데이터 출력 ==");
+        for (int i = 0; i < size; i++) {
+            System.out.println(i + " : " + datum[i]);
+        }
+    }
+}
