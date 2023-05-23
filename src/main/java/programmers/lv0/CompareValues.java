a166066a52b32a8453d5720ce84f9b3621299e83
Jwhyee <kawnsdud@naver.com>
2023-05-23 10:58:32 +0900

    [PRG] LV1 - 체육복

---
+++ src/main/java/programmers/lv0/CompareValues.java
@@ -0,0 +1,20 @@
+package programmers.lv0;
+
+public class CompareValues {
+    public int solution(int a, int b) {
+        String num = String.valueOf(a) + b;
+        int numInt = Integer.parseInt(num);
+        int mul = 2 * a * b;
+        return Math.max(numInt, mul);
+    }
+
+    public static void main(String[] args) {
+        CompareValues c = new CompareValues();
+        int a = 2;
+        int b = 91;
+        System.out.println(c.solution(a, b));
+        a = 91;
+        b = 2;
+        System.out.println(c.solution(a, b));
+    }
+}
---
+++ src/main/java/programmers/lv1/WorkoutClothes.java
@@ -0,0 +1,44 @@
+package programmers.lv1;
+
+public class WorkoutClothes {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] people = new int[n];
+        int answer = n;
+
+        for (int l : lost)
+            people[l-1]--;
+        for (int r : reserve)
+            people[r-1]++;
+
+        for (int i = 0; i < people.length; i++) {
+            if (people[i] == -1) {
+                if (i - 1 >= 0 && people[i - 1] == 1) {
+                    people[i]++;
+                    people[i - 1]--;
+                } else if (i + 1 < people.length && people[i + 1] == 1) {
+                    people[i]++;
+                    people[i + 1]--;
+                } else {
+                    answer--;
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        WorkoutClothes w = new WorkoutClothes();
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {1, 3, 5};
+        System.out.println(w.solution(n, lost, reserve) == 5);
+
+        reserve = new int[]{3};
+        System.out.println(w.solution(n, lost, reserve) == 4);
+
+        n = 3;
+        lost = new int[]{3};
+        reserve = new int[]{1};
+        System.out.println(w.solution(n, lost, reserve) == 2);
+
+    }
+}
