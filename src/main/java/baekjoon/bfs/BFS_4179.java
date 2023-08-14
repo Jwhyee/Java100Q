a8e2eefd117e658bc015bb1c0f98c70fc828cd49
Jwhyee <kawnsdud@naver.com>
2023-08-14 16:38:19 +0900

    [BOJ][GOL4] - 불

--- src/main/java/baekjoon/bfs/BFS_4179.java
+++ src/main/java/baekjoon/bfs/BFS_4179.java
@@ -3,44 +3,46 @@ package baekjoon.bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class BFS_4179 {
-    static char[][] map;
+    static int[][] map;
     static boolean[][] visited;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int R, C;
     static int[] jPos;
-    static List<FireNode> fireList = new ArrayList<>();
+    static Queue<Node> fireQueue = new LinkedList<>();
+    static Queue<Node> jihoonQueue = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] arr = br.readLine().split(" ");
         R = Integer.parseInt(arr[0]);
         C = Integer.parseInt(arr[0]);
 
-        map = new char[R][C];
+        map = new int[R][C];
         visited = new boolean[R][C];
         jPos = new int[2];
 
         for (int i = 0; i < R; i++) {
             char[] charArray = br.readLine().toCharArray();
             for (int j = 0; j < C; j++) {
-                if (charArray[j] == 'J') {
-                    jPos[0] = i;
-                    jPos[1] = j;
+                map[i][j] = 0;
+                if(charArray[j] == '#') {
+                    map[i][j] = -1;
+                } else if(charArray[j] == 'J') {
+                    map[i][j] = 0;
+                    jihoonQueue.offer(new Node(j, i, 0));
+                } else if (charArray[j] == 'F') {
+                    map[i][j] = -2;
+                    fireQueue.offer(new Node(j, i, 0));
                 }
-                if (charArray[j] == 'F') {
-                    fireList.add(new FireNode(j, i));
-                }
-                map[i][j] = charArray[j];
             }
         }
 
-        int result = bfs(jPos[0], jPos[1]);
+        int result = bfs();
 
         if (result == -1) {
             System.out.println("IMPOSSIBLE");
@@ -49,45 +51,52 @@ public class BFS_4179 {
         }
     }
 
-    private static int bfs(int y, int x) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(x, y, 1));
+    private static int bfs() {
+        while (!jihoonQueue.isEmpty()) {
+            Node cur = jihoonQueue.poll();
 
-        while (!queue.isEmpty()) {
-            Node currentPos = queue.poll();
-
+            // 지훈이가 한 칸 이동
             for (int i = 0; i < 4; i++) {
-                int nx = currentPos.x + dx[i];
-                int ny = currentPos.y + dy[i];
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
 
                 if (nx >= 0 && nx < C && ny >= 0 && ny < R) {
-                    if ((map[ny][nx] == '.' && (nx == C - 1 || ny == R - 1))) {
-                        return currentPos.cost + 1;
-                    } else if (map[ny][nx] == '.') {
-                        map[currentPos.y][currentPos.x] = '.';
-                        queue.offer(new Node(nx, ny, currentPos.cost + 1));
+                    if ((map[ny][nx] == 0 && (nx == C - 1 || ny == R - 1))) {
+                        // 한 칸이동 시간 + 탈출 시간
+                        return (cur.cost + 1) + 1;
+                    } else if (map[ny][nx] == 0) {
+                        jihoonQueue.offer(new Node(nx, ny, cur.cost + 1));
                     }
                 }
             }
-            fireBfs();
+
+            // 불이 한 칸씩 퍼짐
+            int fireQueueSize = fireQueue.size();
+            while (fireQueueSize-- > 0) {
+                Node fireNode = fireQueue.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nx = fireNode.x + dx[i];
+                    int ny = fireNode.y + dy[i];
+
+                    if (nx >= 0 && nx < C && ny >= 0 && ny < R) {
+                        if (map[ny][nx] == 0) {
+                            map[ny][nx] = -2;
+                            fireQueue.offer(new Node(nx, ny, 0));
+                        }
+                    }
+                }
+            }
+            showMatrix();
         }
 
         return -1;
     }
 
-    private static void fireBfs() {
-        for (FireNode fireNode : fireList) {
-            for (int i = 0; i < 4; i++) {
-                int nx = fireNode.x + dx[i];
-                int ny = fireNode.y + dy[i];
-
-                if (nx >= 0 && nx < C && ny >= 0 && ny < R) {
-                    if (map[ny][nx] == '.') {
-                        map[ny][nx] = 'F';
-                    }
-                }
-            }
-        }
+    public static void showMatrix() {
+        Arrays.stream(map)
+                .map(Arrays::toString)
+                .forEach(System.out::println);
+        System.out.println("----------------");
     }
 
     static class Node {
@@ -97,15 +106,6 @@ public class BFS_4179 {
             this.x = x;
             this.y = y;
             this.cost = cost;
-        }
-    }
-
-    static class FireNode {
-        int x, y;
-
-        public FireNode(int x, int y) {
-            this.x = x;
-            this.y = y;
         }
     }
 }
