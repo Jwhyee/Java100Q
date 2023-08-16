e1b54d6ed8a774265d38de16a956052be1ca1cf0
Jwhyee <kawnsdud@naver.com>
2023-08-16 21:54:05 +0900

    [BOJ][SIL2] - 5차 전직

---
+++ src/main/java/baekjoon/greedy/Greedy_16112.java
@@ -0,0 +1,46 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Greedy_16112 {
+    static long[] expArr;
+    static int n, k;
+    static ArrayList<Integer> activeStones;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        expArr = new long[n];
+        activeStones = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            expArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(expArr);
+
+        long result = 0;
+        int activeStoneCnt = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (activeStoneCnt < k) {
+                result += expArr[i] * activeStoneCnt;
+                activeStoneCnt++;
+            } else {
+                result += expArr[i] * activeStoneCnt;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
