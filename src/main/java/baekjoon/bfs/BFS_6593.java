b9157c5bf9d963ad46a3e0227313d69f132b75aa
Jwhyee <kawnsdud@naver.com>
2023-08-18 17:19:12 +0900

    refactor : [BOJ][GOL5] - 상범빌딩

--- src/main/java/baekjoon/bfs/BFS_6593.java
+++ src/main/java/baekjoon/bfs/BFS_6593.java
@@ -18,60 +18,48 @@ public class BFS_6593 {
     static Node exitNode;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] arr = br.readLine().split(" ");
-        L = Integer.parseInt(arr[0]);
-        R = Integer.parseInt(arr[1]);
-        C = Integer.parseInt(arr[2]);
-        map = new int[L][R][C];
-        visited = new boolean[L][R][C];
 
-        for (int i = 0; i < L; i++) {
-            for (int j = 0; j < R; j++) {
-                char[] charArr = br.readLine().toCharArray();
-                for (int k = 0; k < C; k++) {
-                    if (charArr[k] == '.') {
-                        map[i][j][k] = 0;
-                    } else if (charArr[k] == 'S') {
-                        map[i][j][k] = 0;
-                        nodeQueue.offer(new Node(i, k, j, 0));
-                    } else if (charArr[k] == '#') {
-                        map[i][j][k] = -1;
-                    } else if (charArr[k] == 'E') {
-                        map[i][j][k] = 0;
-                        exitNode = new Node(i, k, j, 0);
+        while (true) {
+            String[] arr = br.readLine().split(" ");
+            L = Integer.parseInt(arr[0]);
+            R = Integer.parseInt(arr[1]);
+            C = Integer.parseInt(arr[2]);
+
+            if (L == 0 && R == 0 && C == 0) {
+                break;
+            }
+            nodeQueue.clear();
+            map = new int[L][R][C];
+            visited = new boolean[L][R][C];
+
+            for (int i = 0; i < L; i++) {
+                for (int j = 0; j < R; j++) {
+                    char[] charArr = br.readLine().toCharArray();
+                    for (int k = 0; k < C; k++) {
+                        if (charArr[k] == '.') {
+                            map[i][j][k] = 0;
+                        } else if (charArr[k] == 'S') {
+                            map[i][j][k] = 0;
+                            nodeQueue.offer(new Node(i, k, j, 0));
+                        } else if (charArr[k] == '#') {
+                            map[i][j][k] = -1;
+                        } else if (charArr[k] == 'E') {
+                            map[i][j][k] = 0;
+                            exitNode = new Node(i, k, j, 0);
+                        }
                     }
                 }
-            }
-            if (i != L - 1) {
                 br.readLine();
             }
+
+            int result = bfs();
+
+            if (result != -1) {
+                System.out.println("Escaped in " + result + " minute(s).");
+            } else {
+                System.out.println("Trapped!");
+            }
         }
-
-        int result = bfs();
-
-        matrix();
-
-        if (result != -1) {
-            System.out.println("Escaped in " + result + " minute(s).");
-        } else {
-            System.out.println("Trapped!");
-        }
-
-    }
-
-    private static void matrix() {
-        for (int i = 0; i < map.length; i++) {
-            System.out.println("Matrix " + (i + 1) + ":");
-            printMatrix(map[i]);
-            System.out.println();
-        }
-    }
-
-    public static void printMatrix(int[][] map) {
-        Arrays.stream(map)
-                .map(Arrays::toString)
-                .forEach(System.out::println);
-        System.out.println("----------------");
     }
 
     private static int bfs() {
@@ -86,8 +74,6 @@ public class BFS_6593 {
                 int nz = cur.z + dz[i];
 
                 if (nz >= 0 && nz < L) {
-                    System.out.println("---------------");
-                    System.out.println("nz = " + nz);
 
                     for (int j = 0; j < 4; j++) {
                         int nx = cur.x + dx[j];
@@ -95,8 +81,6 @@ public class BFS_6593 {
 
                         if (nx >= 0 && nx < C && ny >= 0 && ny < R) {
                             if (map[nz][ny][nx] == 0 && !visited[nz][ny][nx]) {
-                                System.out.println("ny = " + ny);
-                                System.out.println("nx = " + nx);
                                 if (nz != cur.z) {
                                     map[nz][ny][nx] = cur.cost + 2;
                                     nodeQueue.offer(new Node(nz, nx, ny, cur.cost + 2));
