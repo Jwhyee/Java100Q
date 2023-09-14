128aa8ba73a762ebc1b40e310fe317af7138835d
ka-jun-young <82663161+Jwhyee@users.noreply.github.com>
2023-09-14 16:37:36 +0900

    [Gold IV] Title: 회사 문화 1, Time: 588 ms, Memory: 87808 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.java
@@ -6,47 +6,60 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+/**
+ * 문제 이름(난이도) : 회사 문화1(GOL4)
+ * 시간 : 620 ms
+ * 메모리 : 98220 KB
+ * 링크 : https://www.acmicpc.net/problem/14267
+ */
 public class Main {
     static int N, M;
     static ArrayList<Integer>[] graph;
     static int[] dp;
-    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         // 회사 직원 수
         N = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+
+        // 그래프 초기화
 
         // 최초의 칭찬 횟수
         M = Integer.parseInt(st.nextToken());
 
-        // 직원 그래프 초기화
+        // 그래프 값 초기화
         st = new StringTokenizer(br.readLine());
+
+        graph = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
             // 직속 상사 번호
             int boss = Integer.parseInt(st.nextToken());
+            
+            // 사장의 경우 상사가 없기 때문에 제외
             if (boss == -1) {
                 continue;
             }
+            
+            // 상사의 부하 직원 저장
             graph[boss].add(i);
         }
 
-        // 칭찬 누적 배열
+        // 칭찬 누적 배열 초기화
         dp = new int[N + 1];
         for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
             int employee = Integer.parseInt(st.nextToken());
             int cnt = Integer.parseInt(st.nextToken());
+            
+            // 한 사원이 다른 상사에게도 칭찬 받을 수 있음
             dp[employee] += cnt;
         }
 
-        visited = new boolean[N + 1];
+        // 사장부터 시작
         dfs(1);
 
         for (int i = 1; i <= N; i++) {
@@ -60,6 +73,7 @@ public class Main {
     }
 
     private static void dfs(int employee) {
+        // 직속 상사가 employee인 부하 직원의 칭찬 횟수 구하기
         for (Integer i : graph[employee]) {
             dp[i] += dp[employee];
             dfs(i);
