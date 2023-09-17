05de78546066e3ac91fb5acb7f3e4f6e4eb7d9cb
Jwhyee <kawnsdud@naver.com>
2023-09-17 22:15:30 +0900

    [BOJ][SIL3] - N과 M(2)

--- src/main/java/baekjoon/backtracking/BOJ_15650.java
+++ src/main/java/baekjoon/backtracking/BOJ_15650.java
@@ -1,22 +1,64 @@
 package baekjoon.backtracking;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
+/**
+ * 제목 : N과 M(2) [실버3]
+ * 시간 : 80 ms
+ * 메모리 : 11628 KB
+ * 링크 : https://www.acmicpc.net/problem/15650
+ * */
 public class BOJ_15650 {
+    static int N, M;
+    static boolean[] visited;
+    static int[] values;
+    static BufferedWriter bw;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++
-            ) {
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        values = new int[M];
+        visited = new boolean[N + 1];
+
+        backDfs(0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static boolean backDfs(int depth) throws IOException {
+
+        // 현재 깊이가 출력해야하는 개수와 동일하다면 출력
+        if (depth == M) {
+            for (int value : values) {
+                bw.append(value + " ");
+            }
+            bw.append("\n");
+            return false;
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                values[depth] = i + 1;
+                backDfs(depth + 1);
+                for (int j = i + 1; j <= N; j++) {
+                    visited[j] = false;
+                }
             }
         }
+
+        return true;
     }
 }
