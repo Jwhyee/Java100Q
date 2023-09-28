adf85a331def3f3e5d5409d2f4a1ecc90888d503
ka-jun-young <82663161+Jwhyee@users.noreply.github.com>
2023-09-28 09:50:01 +0900

    [level 2] Title: 가장 큰 수, Time: 1.57 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] answer = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(answer, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if (answer[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : answer) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+
+    }
+}
