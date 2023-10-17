059ecfcb0b3878722a2bd03dff0f66cd79e92098
ka-jun-young <82663161+Jwhyee@users.noreply.github.com>
2023-10-17 14:53:41 +0900

    [Gold V] Title: 치킨 배달, Time: 204 ms, Memory: 15476 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Node {
+    int x, y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    static int N, M, min;
+    static List<Node> homeList, chickenList;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 지도의 크기
+        N = Integer.parseInt(st.nextToken());
+
+        // 치킨집의 수 M <= 13
+        M = Integer.parseInt(st.nextToken());
+
+        // 리스트 초기화
+        homeList = new ArrayList<>();
+        chickenList = new ArrayList<>();
+
+        // 집, 치킨집 리스트에 좌표 추가
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                // 0 : 빈칸 / 1 : 집 / 2 : 치킨집
+                int num = Integer.parseInt(st.nextToken());
+                if (num == 1) homeList.add(new Node(j, i));
+                else if (num == 2) chickenList.add(new Node(j, i));
+            }
+        }
+
+        min = Integer.MAX_VALUE;
+
+        // 한 집에 대해서 치킨집의 좌표를 빼기
+        visited = new boolean[chickenList.size()];
+        backTracking(0, 0);
+
+        bw.append(min + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void backTracking(int depth, int cnt) {
+        // 깊이가 M일 경우 탐색 시작
+        // cnt가 최대에 달하면, 선택된 치킨집을 대상으로 모든 집에 대한 거리를 확인해야함.
+        if (cnt == M) {
+            // 각 집에 대해서 치킨 집의 최소 거리들의 합
+            int sum = 0;
+
+            for (int i = 0; i < homeList.size(); i++) {
+                int temp = Integer.MAX_VALUE;
+                for (int j = 0; j < chickenList.size(); j++) {
+                    if (visited[j]) {
+                        int result = (Math.abs(homeList.get(i).x - chickenList.get(j).x)
+                                + Math.abs(homeList.get(i).y - chickenList.get(j).y));
+                        temp = Math.min(temp, result);
+                    }
+                }
+                sum += temp;
+            }
+            min = Math.min(sum, min);
+            return;
+        }
+
+        for (int i = depth; i < chickenList.size(); i++) {
+            visited[i] = true;
+
+            backTracking(i + 1, cnt + 1);
+
+            visited[i] = false;
+        }
+    }
+}
