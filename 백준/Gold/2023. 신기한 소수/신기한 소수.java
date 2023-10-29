ffc742e5aeebe8a7e6f7ed6a0c93c6e96e048a53
ka-jun-young <82663161+Jwhyee@users.noreply.github.com>
2023-10-29 17:25:07 +0900

    [Gold V] Title: 신기한 소수, Time: 108 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int[] primes = {1, 2, 3, 5, 7, 9}, numbers;
+    static StringBuilder sb;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        numbers = new int[n];
+        sb = new StringBuilder();
+        sc.close();
+
+        backTracking(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void backTracking(int depth, int curNum) {
+        if (depth == n) {
+
+            sb.append(curNum).append("\n");
+
+            return;
+        }
+
+        for (int prime : primes) {
+            if (depth == 0 && prime == 1) {
+                continue;
+            }
+
+            numbers[depth] = prime;
+            int nextNum = curNum(depth);
+            if(isPrime(nextNum))
+                backTracking(depth + 1, nextNum);
+        }
+    }
+
+    private static int curNum(int depth) {
+        int num = 0;
+        for (int i = depth, j = 1; i >= 0; i--, j *= 10) {
+            num += (numbers[i] * j);
+        }
+        return num;
+    }
+
+    private static boolean isPrime(int num) {
+        for (int i = 2; i <= (int) Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
