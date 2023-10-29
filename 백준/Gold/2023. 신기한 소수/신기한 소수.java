65cd533b02ce152b13653f46900bb89edaf07050
ka-jun-young <82663161+Jwhyee@users.noreply.github.com>
2023-10-29 17:25:51 +0900

    [Gold V] Title: 신기한 소수, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,19 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
     static int n;
     static int[] primes = {1, 2, 3, 5, 7, 9}, numbers;
     static StringBuilder sb;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
         numbers = new int[n];
         sb = new StringBuilder();
-        sc.close();
+        br.close();
 
         backTracking(0, 0);
 
         System.out.println(sb);
+
     }
 
     private static void backTracking(int depth, int curNum) {
