f5fd95baeda914381cc62ee0ee0eb35579245db5
ka-jun-young <82663161+Jwhyee@users.noreply.github.com>
2023-10-30 20:07:56 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = """
+                         ,r'"7
+                r`-_   ,'  ,/
+                 \\. ". L_r'
+                   `~\\/
+                      |
+                      |
+                """;
+
+        System.out.println(str);
+    }
+}
