781a56286dfc904269c0475bf86e7bc806f130bc
ka-jun-young <82663161+Jwhyee@users.noreply.github.com>
2023-10-30 13:45:42 +0900

    [Gold II] Title: 트리의 지름, Time: 784 ms, Memory: 100492 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Node>[] graph;
+    static boolean[] visited;
+    static int C, max, leaf;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        C = T;
+
+        graph = new ArrayList[T + 1];
+        visited = new boolean[T + 1];
+
+        for (int i = 1; i <= T; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child;
+            while ((child = Integer.parseInt(st.nextToken())) != -1) {
+                int point = Integer.parseInt(st.nextToken());
+                graph[parent].add(new Node(child, point));
+            }
+        }
+        dfs(1, 0);
+
+
+        visited = new boolean[visited.length];
+        max = 0;
+        dfs(leaf, 0);
+
+        System.out.println(max);
+    }
+
+    private static void dfs(int cur, int point) {
+        visited[cur] = true;
+
+        for (Node node : graph[cur]) {
+            if (!visited[node.num]) {
+                dfs(node.num, point + node.point);
+            }
+        }
+
+        if (max < point) {
+            leaf = cur;
+            max = point;
+        }
+    }
+
+    static class Node {
+        int num, point;
+
+        public Node(int num, int point) {
+            this.num = num;
+            this.point = point;
+        }
+    }
+}
