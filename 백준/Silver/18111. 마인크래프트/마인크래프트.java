a85da5a746c4ac6e8e885c8fe3b9335709189816
ka-jun-young <82663161+Jwhyee@users.noreply.github.com>
2023-11-02 18:58:14 +0900

    [Silver II] Title: 마인크래프트, Time: 564 ms, Memory: 34656 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,11 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
@@ -41,11 +40,13 @@ public class Main {
             }
 
             if(block < 0) break;
+            
             if (time >= cnt) {
                 time = cnt;
                 floor = i;
             }
         }
         System.out.println(time + " " + floor);
+        br.close();
     }
 }
